37354a17db84dd2f6e0c751ce2b4b07372a87fb2
ailin <planet.of.translation@gmail.com>
2017-09-08 13:32:50 +0300

    -Added more test methods

--- src/test/groovy/tweettie/QuoteSpec.groovy
+++ src/test/groovy/tweettie/QuoteSpec.groovy
@@ -1,23 +1,42 @@
 package tweettie
 
 import grails.test.mixin.TestFor
+import spock.lang.Shared
 import spock.lang.Specification
 import twee.Quote
+import twee.User
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Quote)
 class QuoteSpec extends Specification {
+    @Shared
+    User user
 
     def setup() {
+        user = new User(username: "Masha", password: "masha")
     }
+
 
     def cleanup() {
     }
 
-    void "test something"() {
-//        expect:"fix me"
-//            true == false
+    void "test user constraints author balnk content  blank false"() {
+
+        when:
+        def quote = new Quote(author: user, content: content)
+        quote.validate()
+
+        then:
+        quote.hasErrors() == !valid
+
+        where:
+
+        content                     | valid
+        ""                          | false
+        "Some content of the quote" | true
     }
+
+
 }
